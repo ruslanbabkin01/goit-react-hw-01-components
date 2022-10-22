265c88ad04fa385442a588676ef3ff29973f042e
Ruslan Babkin <ruslan.babkin01@gmail.com>
2022-10-22 18:17:14 +0300

    add files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+// import { TransactionHistory } from './Profile/TransactionHistory';
+// import { FriendList } from './Statistics/FriendList';
+import user from '../data/user.json';
+import data from '../data/transactions.json';
+// import friendsList from '../data/friends.json';
+// import transacrions from '../data/transactions.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <Statistics
+        title="Upload stats"
+        stats={data}
+      />
+    </>
   );
 };
+
